19fe08f09cc140db2de6ca6fdadc036f5c3a1bfd
baebug <ufr134@gmail.com>
2021-05-10 00:12:43 +0900

    databinding3

---
+++ src/app/section/section.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section',
+  templateUrl: './section.component.html',
+  styleUrls: ['./section.component.css']
+})
+export class SectionComponent implements OnInit {
+
+  present: string = 'welcome';
+
+  constructor() { }
+
+  startTime($event){
+    this.present = $event;
+  }
+
+
+
+  ngOnInit(): void {
+  }
+
+}
